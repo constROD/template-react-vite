7689295d418466545dff1a7d5507dd4b0f7d969b
constROD <rialcantara21@gmail.com>
2025-02-28 04:45:53 +0800

    chore: update readme

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { cn } from '@/utils/classnames';
+import { cn } from '@/lib/utils';
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
